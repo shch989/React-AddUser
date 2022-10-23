b4f1419c0292bc10a5a1b58c6ec695756137b205
shch989 <tankpop11@gmail.com>
2022-10-23 15:48:52 +0900

    유저 배열 생성 및 에러모델 생성

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 import classes from './AddUser.module.css'
 
@@ -11,7 +12,16 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername, enteredAge)
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return
+    }
+    if(+enteredAge < 0) {
+      return 
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    // console.log(enteredUsername, enteredAge)
+    setEnteredUsername('')
+    setEnteredAge('')
   }
 
   const usernameChangedHandler = (event) => {
@@ -22,15 +32,19 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id='username' type="text" onChange={usernameChangedHandler}></input>
-        <label htmlFor="age">Age (Years)</label>
-        <input id='age' type="number" onChange={ageChangedHandler}></input>
-        <Button type='submit'>Add User</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured!" message="Something went wrong!"/>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id='username' type="text" onChange={usernameChangedHandler} value={enteredUsername}></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input id='age' type="number" onChange={ageChangedHandler} value={enteredAge}></input>
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
+
   );
 };
 
